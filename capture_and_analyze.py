8e58119e56e6b9881945ed2ab70a490cda79e364
silverjoda <heizen.sh@gmail.com>
2021-03-03 13:52:19 +0100

    Added audio

--- capture_and_analyze.py
+++ capture_and_analyze.py
@@ -15,7 +15,6 @@ CAPTURE_BEGIN_HOUR = 0  # 0
 CAPTURE_END_HOUR = 7  # 7
 CAPTURE_SLEEP_INTERVAL_SECS = 60
 
-
 def analyze(pic_array, d):
     N = len(pic_array)
     logging.info("Analyzing image sequence of len={}. ".format(N))
@@ -35,14 +34,12 @@ def analyze(pic_array, d):
     # Save plot to image format
     plt.savefig("analysis/images_Day-{}.png".format(d))
 
-
 def take_picture(camera):
     output = np.empty((camera_resolution[1], camera_resolution[0], 3), dtype=np.uint8)
     camera.capture(output, 'rgb')
     output_float = (output.astype(np.float32) / 255.)
     logging.debug("Took image at {}. Min: {}, Max: {}. ".format(time.time(), output_float.min(), output_float.max()))
     return output_float
-
 
 def capture_every_n_sec_until(camera):
     logging.info("Starting camera capturing with interval {}s until {}'th hour".format(CAPTURE_SLEEP_INTERVAL_SECS,
@@ -54,7 +51,6 @@ def capture_every_n_sec_until(camera):
     logging.info("Collected {} images. ".format(len(pics)))
     return np.array(pics)
 
-
 def start_audio_recording(d):
     logging.info("Starting audio capture in separate thread using 'arecord'. ")
     duration = (CAPTURE_END_HOUR - CAPTURE_BEGIN_HOUR) * 3600
@@ -63,19 +59,16 @@ def start_audio_recording(d):
         shell=True)
     return p
 
-
 def setup_camera():
     logging.info("Setting up camera. ")
     camera = PiCamera()
     camera.resolution = camera_resolution
     return camera
 
-
 def save_images(images, d):
     logging.info("Saving images. ")
     save_dir = "raw_images/images_Day-{}.npy".format(d)
     np.save(save_dir, images)
-
 
 def start_schedule(camera):
     logging.info("Starting schedule. Current time: {}. ".format(datetime.datetime.now()))
@@ -95,7 +88,6 @@ def start_schedule(camera):
         save_images(images, d)
         analyze(images, d)
 
-
 def shoot_and_save(img_path):
     cam = setup_camera()
     pic = take_picture(cam)
@@ -103,13 +95,11 @@ def shoot_and_save(img_path):
     im.save(img_path)
     print(f"Shot and saved image to {img_path}")
 
-
 def test():
     logging.info("Testing. ")
     images = np.random.rand(300, 256, 256)
     analyze(images, 0)
     exit()
-
 
 if __name__ == '__main__':
     data_folders = ["audio", "logs", "raw_images"]
